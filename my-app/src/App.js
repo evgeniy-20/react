78c55fa70c02b9ed627f9af77bfa2ca0360fad2a
evgeniy20 <evgeniy.prokopiv@gmail.com>
2022-11-16 17:25:58 +0200

    creating a site template

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Project/Header';
+import Container from './Project/Container';
+import Footer from './Project/Footer';
+
+function App() {
+  return (
+    <div className='project'>
+      <Header />
+      <Container />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Project/Container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Container() {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Container
